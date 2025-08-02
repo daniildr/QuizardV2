628bc32ce53890a7137bf446c246c32557138620
daniildr <git@daniildr.ru>
2025-08-02 17:58:42 +0300

    v0.2.0-alpha

--- Quizard.Application.LicensingApplication/App.xaml.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Quizard.Application.LicensingApplication;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : System.Windows.Application
-{
-}
